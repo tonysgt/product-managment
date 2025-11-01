b375968b0cf0d163c81d37cda00afc15c22a1d33
Antonino <antoninosegreto.93@gmail.com>
2025-11-01 16:52:55 +0100

    first commit

---
+++ src/main/java/org/tonysgt/dto/CreateProductDto.java
@@ -0,0 +1,43 @@
+package org.tonysgt.dto;
+
+import java.math.BigDecimal;
+
+public class CreateProductDto {
+
+    private String name;
+    private String description;
+    private BigDecimal price;
+    private int quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
